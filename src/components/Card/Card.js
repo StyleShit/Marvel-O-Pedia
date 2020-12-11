6eab2fe5ed421ba709f939aae11f56f819ac476c
Evyatar Daud <evyatar.daud@gmail.com>
2020-12-11 12:10:50 +0200

    Use equal sized thumbnails

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,7 +7,7 @@ function Card({ character: { name, description, thumbnail } })
         <div className="card">
             <div className="card-border" data-description={ description }>
                 <h2>{ name }</h2>
-                <img alt={ name } src={ `${ thumbnail.path }.${ thumbnail.extension }` } />
+                <img alt={ name } src={ `${ thumbnail.path }/standard_fantastic.${ thumbnail.extension }` } />
             </div>
         </div>
     )
