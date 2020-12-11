b4bcaf7bee6bb70e775c8f1e4e649569390fd8b6
Evyatar Daud <evyatar.daud@gmail.com>
2020-12-11 11:43:20 +0200

    Add card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Card.css';
+
+function Card({ character: { name, description, thumbnail } })
+{
+    return (
+        <div className="card">
+            <div className="card-border" data-description={ description }>
+                <h2>{ name }</h2>
+                <img alt={ name } src={ `${ thumbnail.path }.${ thumbnail.extension }` } />
+            </div>
+        </div>
+    )
+}
+
+export default Card;
